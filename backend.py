4440a4699fb8ac220e912411fd0e0cc0ab1ca323
Unknown <zahart76@gmail.com>
2018-11-16 18:35:02 +0200

    stage

--- backend.py
+++ backend.py
@@ -9,19 +9,21 @@ def Get_Tags(films):
         movies.append(ia.search_movie(movie)[0])
     for movie_obj in movies:
         ia.update(movie_obj, info=['keywords'])
-        keywords.append(movie_obj['keywords'])
+        keywords.extend(movie_obj['keywords'])
+    for each in keywords:
+        if keywords.count(each) < 3:
+            keywords.remove(each)
     return keywords
 
 
 def Film_Filter(good_lst, bad_lst):
     films = []
-    best_films = []
+    best_films = set()
     best_films_by_tag = []
     movie_dic = {}
-    rating_dic = {}
-    best_keywords = set(good_lst).difference_update(set(bad_lst))
-    for keyword in list(best_keywords):
-        films.append(ia.get_keyword(keyword))
+    best_keywords = set(good_lst[::40]) - set(bad_lst)
+    for keyword in best_keywords:
+        films.extend(ia.get_keyword(keyword))
     while len(films) > 0:
         film = films[0]
         if films.count(film) not in movie_dic:
@@ -30,20 +32,14 @@ def Film_Filter(good_lst, bad_lst):
         films.remove(film)
     while len(best_films_by_tag) < 3:
         number = max(movie_dic.keys())
-        best_films_by_tag.extend(movie_dic[number])
-    for film in best_films_by_tag:
-        rating = film['rating']
-        if rating not in rating_dic:
-            movie_dic[rating] = []
-        movie_dic[rating].append(film)
-    while len(best_films) < 3:
-        number = max(movie_dic.keys())
-        best_films.extend(movie_dic[number])
+        best_films_by_tag.extend(movie_dic.pop(number))
+    for each in best_films_by_tag:
+        best_films.add(each['title'])
     return best_films
 
 
 def Get_Best_Films(best_films, bad_films):
     good_tags = Get_Tags(best_films)
     bad_tags = Get_Tags(bad_films)
-    best_films = [str(i) for i in Film_Filter(good_tags, bad_tags)]
+    best_films = Film_Filter(good_tags, bad_tags)
     return best_films
